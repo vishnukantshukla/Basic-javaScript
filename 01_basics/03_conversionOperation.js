838fd2c24ca2b35628f70337fe31d2d4b64146a1
vishnukantshukla <92075105+vishnukantshukla@users.noreply.github.com>
2024-04-13 05:43:22 +0000

    ConversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+// let score ="33";
+
+let score="333abc"
+
+console.log(typeof score);
+
+console.log(typeof(score));
+
+
+let valueInNumber= Number(score);
+
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber); // yeh Nan Dega yesliya ham value print karake check karna hoga
+
+
+// in case of boolean
+
+// 1=> true , 0 => false
+// "" => false;
+// "vishnukant" => true;
+
+let someNumber=33;
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+
+console.log(typeof stringNumber); 
