cf48906efc8517c2abf614827878ab01f8f249cd
vishnukantshukla <92075105+vishnukantshukla@users.noreply.github.com>
2024-04-13 05:55:28 +0000

    modified conversion file

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,3 +26,46 @@ let stringNumber = String(someNumber);
 console.log(stringNumber);
 
 console.log(typeof stringNumber); 
+
+
+// ****************************Operations ********************
+
+let value=3;
+let negVa= -value;
+
+console.log(value);
+ 
+console.log(negVa)
+
+console.log(2+2);
+
+// console.log(2-2);
+
+// console.log(2*2);
+
+// console.log(2**3);
+
+// console.log(2%3);
+
+// console.log(2/3);
+
+
+let str="vishnukant"
+
+let str1=" Shukla"
+
+let str2=str+str1
+
+console.log(str2);
+
+
+console.log("1"+2);
+console.log(1 + "2");
+
+console.log(+true);  // interger value yeh dega -> 1
+
+console.log(+""); // yeh 0 dega (because empty string false hota hai)
+
+
+// link to study 
+//http://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
