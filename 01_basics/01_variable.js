a6308c6702ce449e642f522b16dd02ed6fe56fd1
vishnukantshukla <92075105+vishnukantshukla@users.noreply.github.com>
2024-04-13 05:27:11 +0000

    varable and datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,18 @@
+const accountId=1444533;
+let accountEmail="vishnu@gmail.com"
+var accountPassword="1234"
+accountCity="shahjahanpur"
+
+// accountId=2;  // const is not allowed to change
+
+accountEmail="hc@hc.com"
+
+accountPassword="1212123"
+// console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+
+/*
+preferred not use  var because of issue in blocks scope and functional scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";  //treat all JS code as new Version
+// alert(3+3); //we are using nodejs not browswer so we not use this
+
+
+// code readilibility should me high we write code
+console.log(3+3);
+console.log("Vishnukant shukla");
+
+let name="vishnukant shukla"
+let age=20;
+let islogin=false;
+
+// primitive  datatype --> 
+// number range-> 2^53
+// bigint -> larger integer range (when we made facebook website , amazon tec);
+// string-> ""
+// Boolean-> truw/false
+// null-> standalone value(khali hai koye value nhii ha)
+// undefined-> (variable sirf initilize hua hai koye value nhi hai)
+
+// object
+
+console.log(typeof age);
+ console.log(typeof null);
+
+ console.log(typeof  undefined);
